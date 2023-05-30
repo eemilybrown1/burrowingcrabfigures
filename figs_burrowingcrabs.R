b46e7dd6beea4a78d90afd2a421ab5e89ed7a413
Emily Brown <e.brown@oceans.ubc.ca>
2023-05-30 14:51:40 -0400

    post-review figure updates

--- figs_burrowingcrabs.R
+++ figs_burrowingcrabs.R
@@ -4,6 +4,8 @@ library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(ggpubr)
+library(ggpmisc)
+library(gridExtra)
 
 #Figure 2
 fig2data <- read.csv("fig2data.csv")
@@ -18,7 +20,6 @@ fig2data %>%
   xlab("Site number") +
   scale_x_continuous(breaks=seq(0,13,1)) +
   ylab(bquote("Crab burrows per m"^2)) +
-  ggtitle("Burrow density") +
   theme_bw() +
   theme(legend.position="none",
         plot.title = element_text(hjust = 0.5),
@@ -27,7 +28,7 @@ fig2data %>%
         panel.grid.major = element_blank(),
         panel.background = element_blank(), 
         panel.border = element_blank(),
-        text=element_text(family="sans"))
+        text=element_text(family="sans", size = 17))
 
 figure2
   
@@ -36,17 +37,14 @@ figure2
 fig3data <- read.csv("fig3data.csv")
 colnames(fig3data)[1] <- "burrowcount_perm2"
 
-lbl1<- paste("R ^ 2 == 0.2004")
-
 figure3 <- 
   fig3data %>%
   ggplot(aes(x = liveveg_percentcover, y= burrowcount_perm2)) +
   geom_point(color = "black", size = 1.5) +
-  geom_smooth(method = "lm", se = FALSE, color="blue") +
+  stat_poly_line(formula = y~x, se=TRUE) +
+  stat_poly_eq(use_label(c("eq", "P")), formula = y~x, size = 5, label.x = 50) +
   xlab("Live vegetation (% cover)") +
   ylab(bquote("Crab burrows per m"^2)) +
-  coord_cartesian(ylim= c(12, 300), xlim= c(4.75, 100)) +
-  ggtitle("Crab burrows and live vegetation") +
   theme_bw() +
   theme(legend.position="none",
         plot.title = element_text(hjust = 0.5),
@@ -55,40 +53,94 @@ figure3 <-
         panel.grid.major = element_blank(),
         panel.background = element_blank(), 
         panel.border = element_blank(),
-        text=element_text(family="sans")) +
-  stat_regline_equation(label.x = 75, label.y = 270, aes(x=liveveg_percentcover, y=yline,
-                                                         label = after_stat(eq.label))) +
-  annotate("text", x=82, y=260, label = lbl1, parse = TRUE) +
-  geom_ribbon(aes(ymin=lowerCI, ymax=upperCI), linetype = 2, alpha=0.2)
+        text=element_text(family="sans", size = 17)) +
+  coord_cartesian(ylim= c(12, 300), xlim= c(4.75, 100))
+
+
+
+  
+  
+#  stat_regline_equation(label.x = 75, label.y = 270, aes(x=liveveg_percentcover, y=yline,
+#                                                         label = after_stat(eq.label))) +
+#  annotate("text", x=82, y=260, label = lbl1, parse = TRUE)
+#  geom_ribbon(aes(ymin=lowerCI, ymax=upperCI), linetype = 2, alpha=0.2)
+
+figure3
+
+
+###fitting exponential curve
+
+mdl1 <- lm(burrowcount_perm2~liveveg_percentcover, data = fig3data)
+mdl2 <- lm(burrowcount_perm2~liveveg_percentcover + I(liveveg_percentcover^2), data = fig3data)
+mdl3 <- lm(burrowcount_perm2~liveveg_percentcover + I(liveveg_percentcover^2) + I(liveveg_percentcover^3), data = fig3data)
+mdl4 <- lm(burrowcount_perm2~I(liveveg_percentcover^2), data = fig3data)
+
+result <- fig3data
+result$mdl1 <- predict(mdl1, newdata = fig3data)
+result$mdl2 <- predict(mdl2, newdata = fig3data)
+result$mdl3 <- predict(mdl3, newdata = fig3data)
+result$mdl4 <- predict(mdl4, newdata = fig3data)
+
+result <- melt(result, id.vars = "liveveg_percentcover", variable.name = "model",
+               value.name = "fitted")
+
+ggplot(result, aes(x = liveveg_percentcover, y = fitted)) +
+  theme_bw() +
+  geom_point(data = fig3data, aes(x=liveveg_percentcover, y=burrowcount_perm2)) +
+  geom_line(aes(colour = model), size = 1)
+
+
+###LOG TRANSFORMED
+
+fig3data %>%
+  ggplot(aes(x = liveveg_percentcover, y= burrowcount_perm2)) +
+  geom_point(color = "black", size = 1.5) +
+#  geom_smooth(method = "lm", se = TRUE, color = "blue") +
+  xlab("Live vegetation (% cover)") +
+  ylab(bquote("Crab burrows per m"^2)) +
+  scale_y_log10() +
+  theme_bw() +
+  theme(legend.position="none",
+        plot.title = element_text(hjust = 0.5),
+        axis.line = element_line(colour = "black"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.background = element_blank(), 
+        panel.border = element_blank(),
+        text=element_text(family="sans", size = 17)) +
+  stat_poly_line(formula = y~x, se=TRUE) +
+  stat_poly_eq(use_label(c("eq", "P")), size = 5, label.x = 50)
 
 figure3
   
 
 #Figure 4
 fig4data <- read.csv("fig4data.csv")
-colnames(fig4data)[1] <- "date"
+colnames(fig4data) <- c("date", "exclosure02", "exclosure04", "exclosure06", "exclosure08", "exclosure10", "exclosure12")
+fig4data$date <- as.Date(fig4data$date, "%Y-%m-%d")
+
 
 figure4 <- 
 fig4data %>%
   gather(key = "exclosure", value = "crabs", 
-         exclosure12.2, 
-         exclosure12.4, 
-         exclosure12.6, 
-         exclosure13.2,
-         exclosure13.4,
-         exclosure13.6) %>%
+         exclosure02, 
+         exclosure04, 
+         exclosure06, 
+         exclosure08,
+         exclosure10,
+         exclosure12) %>%
   ggplot(aes(x=date, y=crabs, color = exclosure, group = exclosure)) +
   geom_line(linewidth=1) +
   xlab("Date") +
   ylab(bquote("Number of crabs removed per 9 m"^2)) +
   scale_color_manual(name = "Exclosure", 
-                      values = c("exclosure12.2" = "red", 
-                                 "exclosure12.4" = "orange", 
-                                 "exclosure12.6" = "green", 
-                                 "exclosure13.2" = "lightblue", 
-                                 "exclosure13.4" = "blue", 
-                                 "exclosure13.6" = "purple")) +
-  ggtitle("Crab removal through time") +
+                      values = c("exclosure02" = "red", 
+                                 "exclosure04" = "orange", 
+                                 "exclosure06" = "green", 
+                                 "exclosure08" = "lightblue", 
+                                 "exclosure10" = "blue", 
+                                 "exclosure12" = "purple")) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%B") +
   scale_y_continuous(expand = c(0,0), limits = c(0, 150)) +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5),
@@ -98,7 +150,7 @@ fig4data %>%
         panel.background = element_blank(), 
         panel.border = element_blank(),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        text=element_text(family="sans"))
+        text=element_text(family="sans", size = 17))
 figure4
 
 #Figure 5
@@ -107,17 +159,21 @@ colnames(fig5data)[1] <- "burrows"
 
 figure5 <- 
   fig5data %>%
-  gather(key = "plant", value = "pccover", 
+  gather(key = "Species", value = "pccover", 
          salicornia_spp, 
          other_forbs) %>%
-  ggplot(aes(x=burrows, y=pccover, color = plant, group = plant)) +
-  geom_point(size = 2) +
-    labs(color = "") + 
-    xlab("Average # burrows") +
-    ylab("Vegetation % cover") +
+  ggplot(aes(x=burrows, y=pccover, group = Species, shape = Species, color = Species, linetype = Species)) +
+  geom_point(size = 3) +
+  stat_poly_line(formula = y~0+x, se=FALSE) +
+  stat_poly_eq(use_label(c("eq", "P")), formula = y~0+x, size = 5) +
+  xlab("Average # burrows") +
+    ylab("Percent ground cover") +
     scale_color_manual(labels = c("Other forbs", "Salicornia species"),
-                       values = c("blue", "darkorange1")) +
-    ggtitle("Salicornia spp. and Other Forbs by Average Burrow Count") +
+                       values = c("darkorange1", "blue")) +
+    scale_shape_manual(labels = c("Other forbs", "Salicornia species"),
+                       values = c(16, 17)) +
+    scale_linetype_manual(labels = c("Other forbs", "Salicornia species"),
+                          values = c("solid", "dashed")) +
     scale_y_continuous(expand = c(0,0), limits = c(-1, 12)) +
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5),
@@ -126,29 +182,32 @@ figure5 <-
           panel.grid.major = element_blank(),
           panel.background = element_blank(), 
           panel.border = element_blank(),
-          text=element_text(family="sans")) +
-    stat_poly_line(formula = y~0+x, se=FALSE, linetype = "dashed") +
-    stat_poly_eq(use_label(c("eq", "P")), formula = y~0+x, sep = "  ")
+          text=element_text(family="sans", size = 17),
+          legend.key.size = unit(3, "line"))
   
 figure5
   
-#Figure 6
+#Figure 6 
 fig6data <- read.csv("fig6data.csv")
 colnames(fig6data)[1] <- "burrows"
 
 figure6 <- 
   fig6data %>%
-  gather(key = "gcov", value = "pccover", 
+  gather(key = "Cover", value = "pccover", 
          wrack, 
          bare) %>%
-  ggplot(aes(x=burrows, y=pccover, color = gcov, group = gcov)) +
-  geom_point(size = 2) +
-  labs(color = "") + 
+  ggplot(aes(x=burrows, y=pccover, group = Cover, shape = Cover, color = Cover, linetype = Cover)) +
+  geom_point(size = 3) +
+  stat_poly_line(method = "lm", inherit.aes = TRUE, se=FALSE) +
+  stat_poly_eq(use_label(c("eq", "P")), size = 5) +
   xlab("Average # burrows") +
-  ylab("Cover category %") +
+  ylab("Percent ground cover") +
   scale_color_manual(labels = c("Bare", "Wrack"),
                      values = c("darkorange1", "blue")) +
-  ggtitle("Wrack and Bare Ground by Average Burrow Count") +
+  scale_shape_manual(labels = c("Bare", "Wrack"),
+                     values = c(16, 17)) +
+  scale_linetype_manual(labels = c("Bare", "Wrack"),
+                        values = c("solid", "dashed")) +
   scale_y_continuous(expand = c(0,0), limits = c(0, 45)) +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5),
@@ -157,13 +216,17 @@ figure6 <-
         panel.grid.major = element_blank(),
         panel.background = element_blank(), 
         panel.border = element_blank(),
-        text=element_text(family="sans")) +
-#  stat_cor(label.x = 18) +
-#  stat_regline_equation() +
-  stat_poly_line(method = "lm", se=FALSE, linetype = "dashed") +
-  stat_poly_eq(use_label(c("eq", "P")))
+        text=element_text(family="sans", size = 17),
+        legend.key.size = unit(3, "line")) 
 
 figure6
+
+comb5and6 <- ggarrange(plotlist = list(figure5, figure6),
+                       ncol = 2,
+                       nrow = 1,
+                       labels = "auto",
+                       legend = "bottom")
+comb5and6
 
 #Table for salicornia, other forbs (forced through zero), wrack, bare (not forced through zero)
 
@@ -231,12 +294,19 @@ data.frame(cover_category,
            r2,
            p)
 
-grid.table(equationstable)
+gridExtra::grid.table(equationstable)
 
 #Figure 7
 fig7data <- read.csv("fig7data.csv")
+
 colnames(fig7data)[1] <- "treatment"
 
+fig7data <- fig7data %>%
+  mutate(vegspeciescount = rowSums(fig7data[,c(4:13)] != 0))
+
+fig7data <- fig7data[c(1:12),]
+
+###this table got messed up. need to fix
 fig7summary <-
   fig7data %>%
   group_by(treatment) %>%
@@ -252,7 +322,6 @@ figure7 <-
   xlab("Treatment") +
   ylab("Species richness") +
   scale_fill_manual(values = c("blue", "darkorange1")) +
-  ggtitle("Species Richness by Treatment") +
   scale_y_continuous(expand = c(0,0), limits = c(0, 9)) +
   theme_bw() +
   theme(legend.position="none",
@@ -262,7 +331,7 @@ figure7 <-
         panel.grid.major = element_blank(),
         panel.background = element_blank(), 
         panel.border = element_blank(),
-        text=element_text(family="sans"))
+        text=element_text(family="sans", size = 20))
 
 figure7
 
@@ -310,7 +379,6 @@ figure8 <-
                               "bare" = "Bare",
                               "dead" = "Dead",
                               "wrack" = "Wrack")) +
-  ggtitle("Change in Vegetation Species Percent Cover After Crab Exclusion") +
   scale_y_continuous(expand = c(0,0), limits = c(-40, 30)) +
   theme_bw() +
   theme(plot.title = element_text(hjust = 0.5),
@@ -320,6 +388,6 @@ figure8 <-
         panel.border = element_blank(), 
         panel.background = element_blank(),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
-        text=element_text(family="sans"))
+        text=element_text(family="sans", size = 20))
 
 figure8
